8c015e7eb2fda31cf516e071b8887900ec7584bc
Nishkarsh <nishkars@thoughtworks.com>
2016-08-18 23:28:44 +0530

    Using newer version of firebase (added firebase-database as dependency)

--- app/src/main/java/com/example/dell/firebaseandroidpoc/MainActivity.java
+++ app/src/main/java/com/example/dell/firebaseandroidpoc/MainActivity.java
@@ -1,44 +1,46 @@
 package com.example.dell.firebaseandroidpoc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    Firebase mref;
+    private TextView textView;
+    private String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        textView = (TextView) findViewById(R.id.textview_sample);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        textView = (TextView) findViewById(R.id.textview_sample);
-        mref = new Firebase("https://fir-poc-cf52a.firebaseio.com/conditions");
-        mref.addValueEventListener(new ValueEventListener() {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("message");
+        reference.setValue("Hello World!");
+
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
                 String data = dataSnapshot.getValue(String.class);
                 textView.setText(data);
-
-
+                Log.i(TAG, "Got data from Firebase database: " + data);
             }
 
             @Override
-            public void onCancelled(FirebaseError firebaseError) {
-
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "DatabaseError occurred", databaseError.toException());
             }
         });
     }
